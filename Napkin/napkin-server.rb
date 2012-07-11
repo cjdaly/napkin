11700e729224b3efcb9d9414fdc5f2aa013e869c
Dude <dude@example.com>
2012-07-11 01:34:55 -0700

    fred

--- Napkin/napkin-server.rb
+++ Napkin/napkin-server.rb
@@ -1,12 +1,6 @@
+require 'napkin-config'
 require 'rubygems'
 require 'sinatra'
-require 'neo4j'
-
-Neo4j::Config[:storage_path] = "../../Napkin-Data/neo4j-db"
-  
-require 'open-uri/cached'
-
-OpenURI::Cache.cache_path = '../../Napkin-Data/open-uri-cache'
 
 #
 require 'napkin-server-helpers'
