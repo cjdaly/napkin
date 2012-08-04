033082a7e39aabafdc544168e622d986624d4ece
cjdaly <cjdaly@entranched.net>
2012-08-04 14:37:41 -0700

    lots of churn

--- Napkin/napkin-tracker.rb
+++ Napkin/napkin-tracker.rb
@@ -26,7 +26,7 @@ module Napkin
         body_text = @request.body.read
         body_hash = FEED_PROPS.yaml_to_hash(body_text)
 
-        output_text = sub_handle(body_hash)
+        output_text = subclass_handle(body_hash)
         if (output_text.nil?)
           filtered_text = FEED_PROPS.hash_to_yaml(body_hash)
           "!!! HTTP - #{@method}: '#{get_segment}', #{@nn[:id]}\n#{body_text}\n!-->\n#{filtered_text}"
@@ -35,13 +35,13 @@ module Napkin
         end
       end
 
-      def sub_handle(body_hash)
+      def subclass_handle(body_hash)
         return nil
       end
     end
 
     class FeedPostHandler < FeedHandler
-      def sub_handle(body_hash)
+      def subclass_handle(body_hash)
         name = body_hash['name']
         return nil if name.nil?
 
@@ -56,7 +56,7 @@ module Napkin
     end
 
     class FeedPutHandler < FeedHandler
-      def sub_handle(body_hash)
+      def subclass_handle(body_hash)
         ""
       end
     end
@@ -71,15 +71,9 @@ module Napkin
 
       nn.go_sub_path!('tracker/cycle')
       nn.get_or_init('cycle_count',0)
-      nn.get_or_init('pre_cycle_delay_seconds',5)
+      nn.get_or_init('pre_cycle_delay_seconds',9)
+      nn.get_or_init('mid_cycle_delay_seconds',5)
       nn.get_or_init('post_cycle_delay_seconds',1)
-    end
-
-    def init_git
-      @cache_dir = Napkin::Config::OPEN_URI_CACHE_PATH
-      git_command("init")
-      git_command("config --file #{@cache_dir}/.git/config user.name #{Napkin::Config::GIT_USER_NAME}")
-      git_command("config --file #{@cache_dir}/.git/config user.email #{Napkin::Config::GIT_USER_EMAIL}")
     end
 
     def cycle
@@ -95,14 +89,11 @@ module Napkin
           init_git
 
           puts "Tracker thread initialized..."
-
-          @n = Napkin::NodeUtil::NodeNav.new
-          # helper = RssReader.new
           while (next_cycle)
-            puts "Tracker thread - cycle: #{@cycle_count}, path: #{@n.get_path}"
+            puts "Tracker thread - cycle: #{@cycle_count}"
             sleep @pre_cycle_delay_seconds
             if (@enabled)
-              # helper.refresh_feeds(@delay)
+              refresh_feeds
               puts "Tracker thread refreshed..."
             else
               puts "Tracker thread disabled..."
@@ -121,79 +112,50 @@ module Napkin
     def next_cycle
       cycle_start_time = Time.now
 
-      @n.reset
-      @n.go_sub_path('tracker/cycle')
+      nn = Napkin::NodeUtil::NodeNav.new
+      nn.reset
+      nn.go_sub_path('tracker/cycle')
 
-      @cycle_count = @n['cycle_count']
+      @cycle_count = nn['cycle_count']
       @cycle_count += 1
-      @n['cycle_count']= @cycle_count
+      nn['cycle_count']= @cycle_count
 
-      @pre_cycle_delay_seconds = @n['pre_cycle_delay_seconds']
-      @post_cycle_delay_seconds = @n['post_cycle_delay_seconds']
+      @pre_cycle_delay_seconds = nn['pre_cycle_delay_seconds']
+      @mid_cycle_delay_seconds = nn['mid_cycle_delay_seconds']
+      @post_cycle_delay_seconds = nn['post_cycle_delay_seconds']
 
-      @n.go_sub!("#{@cycle_count}")
+      nn.go_sub!("#{@cycle_count}")
 
-      @n['cycle_start_time'] = "#{cycle_start_time}"
-      @n['cycle_start_time_i'] = cycle_start_time.to_i
+      nn['cycle_start_time'] = "#{cycle_start_time}"
+      nn['cycle_start_time_i'] = cycle_start_time.to_i
 
       #
-      puts ">>> CYCLE: #{@n.get_path} / #{@n['cycle_count']}"
+      puts ">>> CYCLE: #{nn.get_path} / #{nn['cycle_count']}"
 
       return true
     end
 
-    def do_git_stuff()
-      git_command("status -s")
-      git_command("add .")
-      git_command("commit -m \"...\"")
-      git_command("status -s")
-      git_command("tag -a loop-#{@cycle_count} -m \"...\"")
-    end
+    def refresh_feeds
+      nn_cycle = Napkin::NodeUtil::NodeNav.new
+      nn_cycle.go_sub_path("tracker/cycle/#{@cycle_count}")
 
-    def git_command(command, cache_dir=@cache_dir, git_dir=@cache_dir + "/.git")
-      command_text = "git --git-dir=#{git_dir} --work-tree=#{cache_dir} #{command}"
-      result_text = `#{command_text}`
-      result_status = $?
-      puts "[exec] git ... #{command}\n  -> #{result_status}\n#{result_text}"
-    end
-  end
-
-  #
-  #
-  class RssReader
-    def refresh_feeds(delay)
-      #      nf = Napkin::NodeUtil::NodeFinder.new
-      #      node = nf.get_sub('feed')
-      #      if (!node.nil?)
-      #        node.outgoing(:sub).each do |sub|
-      #          if sub['refresh_enabled']
-      #            puts "Feed: #{sub[:id]} / #{sub['name']} refreshing..."
-      #            refresh_feed(sub)
-      #            puts "Feed: #{sub[:id]} / #{sub['name']} refreshed..."
-      #          else
-      #            puts "Feed: #{sub[:id]} / #{sub['name']} refresh disabled..."
-      #          end
-      #          sleep delay
-      #        end
-      #      end
+      nn_feeds = Napkin::NodeUtil::NodeNav.new
+      nn_feeds.go_sub_path("tracker/feed")
+      nn_feeds.node.outgoing(:sub).each do |sub|
+        if sub['feed.refresh_enabled']
+          puts "Feed: #{sub[:id]} / #{sub['name']} refreshing..."
+          refresh_feed(sub)
+          puts "Feed: #{sub[:id]} / #{sub['name']} refreshed..."
+          sleep @mid_cycle_delay_seconds
+        else
+          puts "Feed: #{sub[:id]} / #{sub['name']} refresh disabled..."
+        end
+      end
     end
 
     def refresh_feed(feed_node)
-      feed_url=feed_node['url']
-      rss_file_meta_hash = nil
-      open(feed_url) do |rss_file|
-        rss_file_meta_hash = file_meta_to_hash(rss_file.meta)
-      end
-      puts YAML.dump(rss_file_meta_hash)
-    end
-
-    def refresh_feep(feed_id)
       begin
-
-        # TODO: get file meta info, get refresh info, ... time to refresh?
-
-        # TODO:
-        feed_url=nil
+        feed_url=feed_node['feed.url']
 
         rss_file_meta_hash = nil
         rss_text = nil
@@ -201,6 +163,8 @@ module Napkin
           rss_file_meta_hash = file_meta_to_hash(rss_file.meta)
           rss_text = rss_file.read
         end
+
+        puts "RSS file meta:\n#{YAML.dump(rss_file_meta_hash)}"
 
         # TODO:
         # invalidate cache at refresh time
@@ -212,9 +176,11 @@ module Napkin
 
         rss_channel_hash = channel_to_hash(rss.channel)
         # TODO: compare with existing / update
+        puts "RSS Channel:\n#{YAML.dump(rss_channel_hash)}"
 
         rss.items.each do |item|
           rss_item_hash = item_to_hash(item)
+          puts "  RSS Item:\n#{YAML.dump(rss_item_hash)}"
           # TODO: compare with existing / update
         end
       rescue StandardError => err
@@ -245,7 +211,7 @@ module Napkin
       return {
         'title' => rss_item.title,
         'link'=> rss_item.link,
-        'description' => rss_item.description,
+        # 'description' => rss_item.description,
         'guid' => rss_item.guid.content,
         'pubDate' => rss_item.pubDate
       }
@@ -259,5 +225,32 @@ module Napkin
     #    puts attr + ": " + meta[attr]
     #    puts (d2 * 24 * 60).to_i
     #  end
+
+    #
+    # Git stuff
+    #
+
+    def init_git
+      @cache_dir = Napkin::Config::OPEN_URI_CACHE_PATH
+      git_command("init")
+      git_command("config --file #{@cache_dir}/.git/config user.name #{Napkin::Config::GIT_USER_NAME}")
+      git_command("config --file #{@cache_dir}/.git/config user.email #{Napkin::Config::GIT_USER_EMAIL}")
+    end
+
+    def do_git_stuff()
+      git_command("status -s")
+      git_command("add .")
+      git_command("commit -m \"...\"")
+      git_command("status -s")
+      git_command("tag -a cycle_#{@cycle_count} -m \"...\"")
+    end
+
+    def git_command(command, cache_dir=@cache_dir, git_dir=@cache_dir + "/.git")
+      command_text = "git --git-dir=#{git_dir} --work-tree=#{cache_dir} #{command}"
+      result_text = `#{command_text}`
+      result_status = $?
+      puts "[exec] git ... #{command}\n  -> #{result_status}\n#{result_text}"
+    end
   end
+
 end
--- Napkin/test/test-rest-client2.rb
+++ Napkin/test/test-rest-client2.rb
@@ -2,19 +2,31 @@ require 'yaml'
 require 'rubygems'
 require 'rest_client'
 
-def put_node(path, property_hash, hostname="localhost", creds="fred:fred")
+def put_node(
+  path,
+  property_hash,
+  hostname="localhost",
+  creds="fred:fred@",
+  port=":4567"
+  )
   yaml = YAML.dump(property_hash)
   puts "PUT Outgoing:\n#{yaml}\n"
 
-  response = RestClient.put("http://#{creds}@#{hostname}:4567/#{path}", yaml)
+  response = RestClient.put("http://#{creds}#{hostname}#{port}/#{path}", yaml)
   puts "PUT Incoming:\n#{response.to_s()}\n"
 end
 
-def post_node(path, property_hash, hostname="localhost", creds="fred:fred")
+def post_node(
+  path,
+  property_hash,
+  hostname="localhost",
+  creds="fred:fred@",
+  port=":4567"
+  )
   yaml = YAML.dump(property_hash)
   puts "POST Outgoing:\n#{yaml}\n"
 
-  response = RestClient.post("http://#{creds}@#{hostname}:4567/#{path}", yaml)
+  response = RestClient.post("http://#{creds}#{hostname}#{port}/#{path}", yaml)
   puts "POST Incoming:\n#{response.to_s()}\n"
 end
 
@@ -23,4 +35,19 @@ props = {
   'url' => "blah2",
   'quest' => "???"
 }
-post_node('tracker/feed', props)
+
+props_zh = {
+  'name' =>"Zerohedge",
+  'url' => "http://www.zerohedge.com/fullrss2.xml",
+  'refresh_enabled' => true,
+  'refresh_in_minutes' => 20
+}
+
+props_zh2 = {
+  'name' =>"Zerohedge (FeedBurner)",
+  'url' => "http://feeds.feedburner.com/zerohedge/feed?format=xml",
+  'refresh_enabled' => true,
+  'refresh_in_minutes' => 20
+}
+
+post_node('tracker/feed', props_zh)
