bbef492881b19b4c4fc34c27e4f9dd1cb8ab9f67
cjdaly <cjdaly@entranched.net>
2012-10-05 22:30:36 -0700

    sync up with common .Net MF project

--- Napkin-Gadgeteer/CerberusChatterer/CerberusChatterer/Program.cs
+++ Napkin-Gadgeteer/CerberusChatterer/CerberusChatterer/Program.cs
@@ -18,6 +18,7 @@ using GTM = Gadgeteer.Modules;
 using Gadgeteer.Modules.GHIElectronics;
 using Gadgeteer.Modules.Seeed;
 
+using NapkinCommon;
 
 namespace CerberusChatterer
 {
@@ -44,7 +45,7 @@ namespace CerberusChatterer
             NetworkInterface[] nifs = NetworkInterface.GetAllNetworkInterfaces();
             foreach (NetworkInterface nif in nifs)
             {
-                Debug.Print("ip: " + nif.IPAddress + ", dhcp: " +  nif.IsDhcpEnabled);
+                Debug.Print("ip: " + nif.IPAddress + ", dhcp: " + nif.IsDhcpEnabled);
                 if (!nif.IsDhcpEnabled) nif.EnableDhcp();
             }
 
@@ -68,9 +69,9 @@ namespace CerberusChatterer
 
             try
             {
-                NetworkCredential credential = new NetworkCredential("ndp1", "ndp1");
+                NetworkCredential credential = new NetworkCredential("cerb1", "cerb1");
                 string uri = "http://192.168.2.50:4567/napkin/starts";
-                string responseText = DoHttpMethod("GET", uri, credential, null);
+                string responseText = HttpUtil.DoHttpMethod("GET", uri, credential, null);
                 Debug.Print(responseText);
             }
             catch (Exception ex)
@@ -81,54 +82,6 @@ namespace CerberusChatterer
             {
                 Mainboard.SetDebugLED(false);
             }
-        }
-
-        private static string DoHttpMethod(string method, string uri, NetworkCredential credential, string requestText)
-        {
-            string responseText = null;
-
-            using (HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri))
-            {
-                request.Method = method;
-                request.Credentials = credential;
-
-                if (requestText != null)
-                {
-                    byte[] buffer = Encoding.UTF8.GetBytes(requestText);
-                    request.ContentLength = buffer.Length;
-                    request.ContentType = "text/plain";
-
-                    Stream stream = request.GetRequestStream();
-                    stream.Write(buffer, 0, buffer.Length);
-                }
-
-                responseText = GetResponseText(request);
-            }
-
-            return responseText;
-        }
-
-        private static string GetResponseText(HttpWebRequest request)
-        {
-            string responseText = "";
-
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            {
-                int contentLength = (int)response.ContentLength;
-                byte[] buffer = new byte[contentLength];
-                Stream stream = response.GetResponseStream();
-                int i = 0;
-                while (i < contentLength)
-                {
-                    int readCount = stream.Read(buffer, i, contentLength - i);
-                    i += readCount;
-                }
-
-                char[] responseChars = Encoding.UTF8.GetChars(buffer);
-                responseText = new string(responseChars);
-            }
-
-            return responseText;
         }
     }
 }
