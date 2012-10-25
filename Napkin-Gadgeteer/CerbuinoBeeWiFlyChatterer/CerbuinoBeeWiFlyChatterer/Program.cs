4e113b2b05efe4c1409d0e34124d94df851647ac
cjdaly <cjdaly@entranched.net>
2012-10-25 01:44:28 -0700

    trying to make wifly talk.  so far unsuccessfully....

--- Napkin-Gadgeteer/CerbuinoBeeWiFlyChatterer/CerbuinoBeeWiFlyChatterer/Program.cs
+++ Napkin-Gadgeteer/CerbuinoBeeWiFlyChatterer/CerbuinoBeeWiFlyChatterer/Program.cs
@@ -24,16 +24,60 @@ namespace CerbuinoBeeWiFlyChatterer
         {
             Debug.Print("Hello from: " + DeviceId);
 
-            WiFlyGSX wifly = new WiFlyGSX();
-            wifly.EnableDHCP();
-            wifly.JoinNetwork("WIFI24Gb", 0, WiFlyGSX.AuthMode.WPA2_PSK, "Batty$nackH0g");
+            _cycleThread = new Thread(CycleDriver);
+            _cycleThread.Start();
+        }
+
+        private readonly int _cycleDelayMillisecondsInitial = 15 * 1000;
+        private readonly int _cycleDelayMilliseconds = 15 * 1000;
+        private Thread _cycleThread;
+
+        private int _cycleCount = 0;
+
+        private WiFlyGSX _wifly;
+
+        private void CycleDriver()
+        {
+            Debug.Print("cycle thread starting!");
+            Thread.Sleep(_cycleDelayMillisecondsInitial);
+
+            JoinNetwork();
+
+            Debug.Print("Starting cycle: " + _cycleCount + " on device: " + DeviceId);
+
+            bool exit = false;
+            while (!exit)
+            {
+                Thread.Sleep(_cycleDelayMilliseconds);
+                Cycle();
+            }
+        }
+
+        private void Cycle()
+        {
+            _cycleCount++;
+
+            Debug.Print("Starting cycle: " + _cycleCount + " on device: " + DeviceId);
+
+            PingServer();
+        }
+
+        private void JoinNetwork()
+        {
+            _wifly = new WiFlyGSX();
+            _wifly.EnableDHCP();
+            _wifly.JoinNetwork("WIFI24Gb", 0, WiFlyGSX.AuthMode.WPA2_PSK, "Batty$nackH0g");
 
             Debug.Print("joined network");
+        }
 
-            WiFlySocket socket = new WiFlySocket("192.168.2.50", 4567, wifly);
+        private void PingServer()
+        {
+            // WiFlySocket socket = new WiFlySocket("192.168.2.50", 4567, _wifly);
+            WiFlySocket socket = new WiFlySocket("google.com", 80, _wifly);
             HTTP_Client client = new HTTP_Client(socket);
-            client.Authenticate(DeviceId, DeviceId);
-            HTTP_Client.HTTP_Response response = client.Get("/config/cerbee2");
+            // client.Authenticate(DeviceId, DeviceId);
+            HTTP_Client.HTTP_Response response = client.Get("/");
 
             Debug.Print("got from server:");
             Debug.Print(response.ResponseBody);
