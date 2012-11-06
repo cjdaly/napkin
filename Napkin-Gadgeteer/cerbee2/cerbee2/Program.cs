26dabe027a193d258db6581605cf5308cb92489f
cjdaly <cjdaly@entranched.net>
2012-11-05 23:45:41 -0800

    cerbee2 / SD card read attempt

---
+++ Napkin-Gadgeteer/cerbee2/cerbee2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.IO;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+
+namespace cerbee2
+{
+    public partial class Program
+    {
+        void ProgramStarted()
+        {
+            string[] vols = Mainboard.GetStorageDeviceVolumeNames();
+            foreach (string vol in vols)
+            {
+                Debug.Print("volume: " + vol);
+                bool result = Mainboard.MountStorageDevice(vol);
+                Debug.Print("mounted: " + result);
+            }
+
+            VolumeInfo[] volInfos = VolumeInfo.GetVolumes();
+            foreach (VolumeInfo volInfo in volInfos)
+            {
+                string rootDir = volInfo.RootDirectory;
+                Debug.Print("root dir: " + rootDir);
+
+                string[] fileNames = Directory.GetFiles(rootDir);
+                foreach (string fileName in fileNames)
+                {
+                    Debug.Print("file name: " + fileName);
+
+                    String filePath = Path.Combine(rootDir, fileName);
+                    Debug.Print("file path: " + fileName);
+
+                    string fileText = GetFileText(filePath);
+                    Debug.Print(">>>");
+                    Debug.Print(fileText);
+                    Debug.Print("<<<");
+                }
+            }
+
+            Debug.Print("Program Started");
+        }
+
+        private string GetFileText(string filePath)
+        {
+            byte[] bytes = File.ReadAllBytes(filePath);
+            char[] chars = Encoding.UTF8.GetChars(bytes);
+            string fileText = new String(chars);
+            return fileText;
+        }
+    }
+}
