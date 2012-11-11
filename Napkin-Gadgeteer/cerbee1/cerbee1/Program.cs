5dcd135b8c66c816d1b892e636f0d9d71c3752cb
cjdaly <cjdaly@entranched.net>
2012-11-11 13:38:47 -0800

    cerbee1 makeover

---
+++ Napkin-Gadgeteer/cerbee1/cerbee1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+
+using NapkinCommon;
+using NapkinGadgeteerCommon;
+using NapkinGadgeteerCommon.SensorUtil;
+using Gadgeteer.Modules.Seeed;
+using Gadgeteer.Modules.GHIElectronics;
+
+namespace cerbee1
+{
+    public partial class Program
+    {
+        public readonly string DeviceId = "cerbee1";
+        public readonly string NapkinServerUri = "http://192.168.2.50:4567";
+        private NetworkCredential _credential;
+
+        private SamplerBag _samplers = new SamplerBag();
+        private readonly int _cycleDelayMillisecondsInitial = 15 * 1000;
+        private Thread _cycleThread;
+
+        private DeviceVitals _vitals;
+
+        void ProgramStarted()
+        {
+            Debug.Print("Hello from: " + DeviceId);
+
+            _credential = new NetworkCredential(DeviceId, DeviceId);
+
+            new TemperatureHumiditySampler(temperatureHumidity, _samplers);
+            new LightSensorSampler(lightsensor, _samplers);
+
+            _vitals = new DeviceVitals(NapkinServerUri, DeviceId, _credential);
+
+            _cycleThread = new Thread(CycleDriver);
+            _cycleThread.Start();
+        }
+
+        private void CycleDriver()
+        {
+            Debug.Print("cycle thread starting!");
+            Thread.Sleep(_cycleDelayMillisecondsInitial);
+
+            int cycleCount = _vitals.CycleCount;
+            Debug.Print("Starting cycle: " + cycleCount + " on device: " + DeviceId);
+            _samplers.Sample("memory");
+
+            _vitals.UpdateDeviceStarts();
+            _vitals.UpdateDeviceLocation();
+            _vitals.InitPostCycle();
+            _vitals.InitCycleDelayMilliseconds();
+            _samplers.Sample("memory");
+
+            bool exit = false;
+            while (!exit)
+            {
+                Thread.Sleep(_vitals.CycleDelayMilliseconds);
+                Cycle();
+            }
+
+        }
+
+        private void Cycle()
+        {
+            _vitals.IncrementCycleCount();
+            int cycleCount = _vitals.CycleCount;
+            Debug.Print("Starting cycle: " + cycleCount + " on device: " + DeviceId + " with postCycle: " + _vitals.PostCycle);
+            _samplers.Sample("memory");
+
+            if (cycleCount % _vitals.PostCycle == 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                _vitals.AppendStatus(sb);
+                _samplers.AppendStatus(sb);
+                string chatterRequestText = sb.ToString();
+
+                string chatterUri = NapkinServerUri + "/chatter";
+                HttpUtil.DoHttpMethod("POST", chatterUri, _credential, chatterRequestText, false);
+
+                _samplers.Reset();
+                _samplers.Sample("memory");
+            }
+
+            _samplers.Sample("light_sensor_percentage");
+            _samplers.Sample("light_sensor_voltage");
+            _samplers.Sample("memory");
+
+            temperatureHumidity.RequestMeasurement();
+        }
+    }
+}
