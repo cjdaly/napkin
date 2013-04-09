d91c270ff826a2358f3cc7ff3980b7be20420b5a
cjdaly <cjdaly@entranched.net>
2013-04-09 15:38:07 -0700

    leaps and bounds

--- server/napkin-helpers.rb
+++ server/napkin-helpers.rb
@@ -7,7 +7,12 @@ module Napkin
     
     def Helpers.init_neo4j()
       puts "init_neo4j!"
-      Neo.get_sub!('test')
+      napkin_id = Neo.get_sub_id!('napkin')
+      starts_id = Neo.get_sub_id!('starts', napkin_id)
+      cycles_id = Neo.get_sub_id!('cycles', napkin_id)
+      
+      start_count = Neo.increment_counter('napkin.starts.count', starts_id)
+      puts "STARTS: #{start_count}"
     end
     
     def handle_request (path, request, user)
@@ -15,13 +20,18 @@ module Napkin
       segments = path.split('/')
       
       current_segment_index = 0
+      current_node_id = 0
       segments.each_with_index do |segment, i|
         next if segment.to_s.empty?
         
-        Neo.get_sub(segment)
-        
+        sub_node_id = Neo.get_sub_id(segment, current_node_id)
+        if (!sub_node_id.nil?) then
+          current_node_id = sub_node_id
+          puts "SUB: #{segment} : #{current_node_id} in #{path}"
+        else
+          puts "No SUB: #{segment} in #{path}"
+        end
       end
-      
       
       response_text = "#{path} #{request.request_method} #{user}\n"
       return response_text
--- server/neo4j-util.rb
+++ server/neo4j-util.rb
@@ -40,7 +40,51 @@ module Napkin
       return response_json_object
     end
 
-    def Neo4jUtil.get_sub!(sub_node_segment, sup_node_id = 0)
+    def Neo4jUtil.get_property(key, node_id)
+      # TODO: validate key as segment
+      cypher_get_property = {
+        "query" => "START n=node({node_id}) RETURN n.`#{key}`?",
+        "params" => {
+        "node_id" => node_id,
+        }
+      }
+      raw = Neo4jUtil.post(SRC, cypher_get_property)
+      return Neo4jUtil::extract_cypher_result(raw['data'])
+    end
+
+    def Neo4jUtil.set_property(key, value, node_id)
+      # TODO: validate key as segment
+      cypher_set_property = {
+        "query" => "START n=node({node_id}) SET n.`#{key}`={value} RETURN null",
+        "params" => {
+        "node_id" => node_id,
+        "value" => value,
+        }
+      }
+      raw = Neo4jUtil.post(SRC, cypher_set_property)
+      return nil
+    end
+
+    def Neo4jUtil.increment_counter(key, node_id)
+      value = Neo4jUtil.get_property(key, node_id)
+      puts "VAL: #{value}, #{value.class}, #{value.to_json}"
+
+      if (value.nil?) then
+        value = 0
+      end
+
+      puts "VAL: #{value}, #{value.class}, #{value.to_json}"
+      if (value.is_a? Numeric) then
+        value += 1
+        Neo4jUtil.set_property(key, value, node_id)
+      else
+        value = nil
+      end
+      return value
+    end
+
+    def Neo4jUtil.get_sub_id!(sub_node_segment, sup_node_id = 0)
+      # TODO: validate sub_node_segment
       cypher_create_unique = {
         "query" => 'START sup=node({sup_node_id}) CREATE UNIQUE sup-[:SUB]->(sub {`napkin.segment` : {sub_node_segment}}) RETURN ID(sub)',
         "params" => {
@@ -48,10 +92,12 @@ module Napkin
         "sub_node_segment" => sub_node_segment
         }
       }
-      return Neo4jUtil.post(SRC, cypher_create_unique)
+      raw = Neo4jUtil.post(SRC, cypher_create_unique)
+      return Neo4jUtil::extract_cypher_result(raw['data'])
     end
 
-    def Neo4jUtil.get_sub(sub_node_segment, sup_node_id = 0)
+    def Neo4jUtil.get_sub_id(sub_node_segment, sup_node_id = 0)
+      # TODO: validate sub_node_segment
       cypher_get_sub = {
         "query" => 'START sup=node({sup_node_id}) MATCH sup-[:SUB]->sub WHERE sub.`napkin.segment`! = {sub_node_segment} RETURN ID(sub)',
         "params" => {
@@ -59,7 +105,20 @@ module Napkin
         "sub_node_segment" => sub_node_segment
         }
       }
-      return Neo4jUtil.post(SRC, cypher_get_sub)
+      raw =  Neo4jUtil.post(SRC, cypher_get_sub)
+      return extract_cypher_result(raw['data'])
+    end
+
+    #internal helpers
+
+    def Neo4jUtil.extract_cypher_result(raw_data)
+      if (raw_data.length == 1) then
+        data_item = raw_data[0]
+        if ((data_item.is_a? Array) && (data_item.length == 1)) then
+          return data_item[0]
+        end
+      end
+      return nil
     end
 
   end
