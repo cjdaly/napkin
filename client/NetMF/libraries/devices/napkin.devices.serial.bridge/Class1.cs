b815a5273a81d4492b6d1f2eee9955b7babcd8d2
cjdaly <cjdaly@entranched.net>
2013-04-15 01:05:28 -0700

    add copyright and EPL (part 4)

--- client/NetMF/libraries/devices/napkin.devices.serial.bridge/Class1.cs
+++ client/NetMF/libraries/devices/napkin.devices.serial.bridge/Class1.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/****
+ * Copyright (c) 2013 Chris J Daly (github user cjdaly)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   cjdaly - initial API and implementation
+ ****/
+using System;
 using Microsoft.SPOT;
 
 namespace napkin.devices.serial.bridge
