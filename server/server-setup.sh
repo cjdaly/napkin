123bf4e4140adbf6955ce3750a90b024a503ad89
cjdaly <cjdaly@entranched.net>
2013-10-18 12:43:12 -0700

    moving to new versions of java, neo4j, jruby

--- server/server-setup.sh
+++ server/server-setup.sh
@@ -11,31 +11,28 @@
 
 # add Java to $PATH
 echo
-if ! which java &> /dev/null ; then
-  echo "Adding java to PATH"
-  export JAVA_HOME=~/java/ejre1.7.0_21
-  export PATH=$JAVA_HOME/bin:$PATH
-fi
+echo "Adding java to PATH"
+case `uname -i` in
+  x86*) JAVA_HOME="~/java/jdk1.7.0_45";;
+  arm*) JAVA_HOME="~/java/ejre1.7.0_45";;
+esac
+export PATH=$JAVA_HOME/bin:$PATH
 echo "Java version"
 java -version
 
 # add Neo4j to $PATH
 echo
-if ! which neo4j &> /dev/null ; then
-  echo "Adding neo4j to PATH"
-  export NEO4J_HOME=~/neo4j/neo4j-community-1.9.1
-  export PATH=$NEO4J_HOME/bin:$PATH
-fi
+echo "Adding neo4j to PATH"
+export NEO4J_HOME="~/neo4j/neo4j-community-2.0.0-M06"
+export PATH=$NEO4J_HOME/bin:$PATH
 echo "Neo4j status"
 neo4j status
 
 # add JRuby to $PATH
 echo
-if ! which jruby &> /dev/null ; then
-  echo "Adding jruby to PATH"
-  export JRUBY_HOME=~/jruby/jruby-1.7.3
-  export PATH=$JRUBY_HOME/bin:$PATH
-fi
+echo "Adding jruby to PATH"
+export JRUBY_HOME="~/jruby/jruby-1.7.5"
+export PATH=$JRUBY_HOME/bin:$PATH
 echo "JRuby version (wait for it...)"
 jruby -v
 
