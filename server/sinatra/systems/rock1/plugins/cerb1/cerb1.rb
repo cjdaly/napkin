6c96820d26abc070afef599abf234e0088b0600a
cjdaly <cjdaly@entranched.net>
2014-03-02 00:15:21 -0800

    plugin for cerb1 sensor data

---
+++ server/sinatra/systems/rock1/plugins/cerb1/cerb1.rb
@@ -0,0 +1,49 @@
+####
+# Copyright (c) 2014 Chris J Daly (github user cjdaly)
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   cjdaly - initial API and implementation
+####
+
+module Napkin::Plugins
+  class Cerb1 < PluginBase
+    DEVICE_ID = "cerb1"
+    CHATTER_KEY_PREFIXES = [
+      "vitals.",
+      "sensor.temperatureHumidity.",
+      "sensor.lightSensor.",
+      "sensor.barometer."
+    ]
+
+    #
+    def init
+      # service_node_id = init_service_segment
+      register_task('cerb1_sensor_data', SensorData_Task)
+      puts "In Cerb1.init() !!!"
+    end
+
+    class SensorData_Task < Napkin::Tasks::ActiveTaskBase
+      include Napkin::Util::Client
+      def init
+        puts "In Cerb1.SensorData_Task.init() !!!"
+        @sensor_uart = File.open("/dev/ttyS0", "r")
+        @sensor_data = {}
+        increment_start_count(DEVICE_ID)
+      end
+
+      def fini
+        puts "In Cerb1.SensorData_Task.fini() !!!"
+        @sensor_uart.close
+      end
+
+      def cycle
+        process_sensor_data(@sensor_uart, @sensor_data, DEVICE_ID, CHATTER_KEY_PREFIXES)
+      end
+
+    end
+  end
+end
