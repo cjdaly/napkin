b35e50fddcd6bd5ff00fb2b219e554b2a75005e9
cjdaly <cjdaly@entranched.net>
2014-04-14 23:25:48 -0700

    handle bone_capemgr vagaries

--- client/ruby/bone2-cerbee1/setup/initialize_uarts.sh
+++ client/ruby/bone2-cerbee1/setup/initialize_uarts.sh
@@ -1,6 +1,15 @@
-#/bin/bash
+####
+# Copyright (c) 2014 Chris J Daly (github user cjdaly)
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   cjdaly - initial API and implementation
+####
 
 cd /lib/firmware
-echo ttyO1_armhf.com > /sys/devices/bone_capemgr.8/slots
-echo ttyO2_armhf.com > /sys/devices/bone_capemgr.8/slots
-
+SLOTS_DIR="/sys/devices/bone_capemgr.*/slots"
+echo ttyO1_armhf.com > $SLOTS_DIR
+echo ttyO2_armhf.com > $SLOTS_DIR
