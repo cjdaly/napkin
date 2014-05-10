f9f517483561248aa5afe46c21b2ce77ce689042
cjdaly <cjdaly@entranched.net>
2014-05-10 16:39:31 -0700

    clean up LCD output on pcduino1-cerb2

--- client/NetMF/systems/gadgeteer/napkin.systems.gadgeteer.cerb2-pcduino1/Program.cs
+++ client/NetMF/systems/gadgeteer/napkin.systems.gadgeteer.cerb2-pcduino1/Program.cs
@@ -50,19 +50,23 @@ namespace napkin.systems.gadgeteer.cerb2_pcduino1
         }
 
         private double _temperature2 = 0;
+        private double _temperature2F = 0;
         private double _relativeHumidity = 0;
         void temperatureHumidity_MeasurementComplete(TemperatureHumidity sender, double temperature, double relativeHumidity)
         {
             _temperature2 = temperature;
+            _temperature2F = _temperature2 * 1.8 + 32.0;
             _relativeHumidity = relativeHumidity;
         }
 
         private double _temperature = 0;
+        private double _temperatureF = 0;
         private double _pressure = 0;
         void barometer_MeasurementComplete(Barometer sender, Barometer.SensorData sensorData)
         {
             _pressure = sensorData.Pressure;
             _temperature = sensorData.Temperature;
+            _temperatureF = _temperature * 1.8 + 32.0;
         }
 
         private string _lastLine = "";
@@ -79,26 +83,16 @@ namespace napkin.systems.gadgeteer.cerb2_pcduino1
             bool exit = false;
             while (!exit)
             {
-                Thread.Sleep(5000);
+                Thread.Sleep(1000);
                 Cycle();
             }
-        }
-
-        private void RefreshLcd(string line1, string line2 = "")
-        {
-            char_Display.Clear();
-            char_Display.CursorHome();
-            char_Display.PrintString(line1);
-            char_Display.SetCursor(1, 0);
-            char_Display.PrintString(line2);
         }
 
         private void Cycle()
         {
             _cycleCount++;
-            Debug.Print("Cycle: " + _cycleCount);
-
-            RefreshLcd("cycle: " + _cycleCount);
+            Debug.Print("sensor cycle: " + _cycleCount);
+            UpdateDisplay("sensor cycle", _cycleCount.ToString());
 
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("state.vitalsAndSensorsUpdated=false");
@@ -111,35 +105,66 @@ namespace napkin.systems.gadgeteer.cerb2_pcduino1
             Thread.Sleep(500);
             sb.AppendLine("sensor.temperatureHumidity.temperature~f=" + _temperature2.ToString());
             sb.AppendLine("sensor.temperatureHumidity.relativeHumidity~f=" + _relativeHumidity.ToString());
-            RefreshLcd("t:" + _temperature2, "h:" + _relativeHumidity);
 
             _temperature = 0; _pressure = 0;
             barometer.RequestMeasurement();
-            Thread.Sleep(1500);
+            Thread.Sleep(500);
             sb.AppendLine("sensor.barometer.temperature~f=" + _temperature.ToString());
             sb.AppendLine("sensor.barometer.pressure~f=" + _pressure.ToString());
-            RefreshLcd("t:" + _temperature, "p:" + _pressure);
 
-            Thread.Sleep(2500);
             double gasSenseVoltage = gasSense.ReadVoltage();
             sb.AppendLine("sensor.gasSense.MQ-3.voltage~f=" + gasSenseVoltage.ToString());
+            Thread.Sleep(200);
 
             double lightSensorPercentage = lightSensor.ReadLightSensorPercentage();
             sb.AppendLine("sensor.lightSensor.lightSensorPercentage~f=" + lightSensorPercentage.ToString());
-
-            RefreshLcd("g:" + gasSenseVoltage, "l:" + lightSensorPercentage);
 
             long memoryBytesFree = Debug.GC(false);
             sb.AppendLine("vitals.memoryBytesFree~i=" + memoryBytesFree);
 
             sb.AppendLine("sensor.lastLine=" + _lastLine);
+
             sb.AppendLine("state.vitalsAndSensorsUpdated=true");
 
             string chatterText = sb.ToString();
             _pcduino1.Write(chatterText);
 
-            Debug.Print(chatterText);
-            Thread.Sleep(3000);
+            UpdateDisplay("temperature 1", _temperatureF.ToString("n2") + "F / " + _temperature.ToString("n2") + "C");
+            Thread.Sleep(2 * 1000);
+
+            UpdateDisplay("temperature 2", _temperature2F.ToString("n2") + "F / " + _temperature2.ToString("n2") + "C");
+            Thread.Sleep(2 * 1000);
+
+            UpdateDisplay("humidity", _relativeHumidity.ToString("n2") + "%");
+            Thread.Sleep(2 * 1000);
+
+            UpdateDisplay("barometer", _pressure.ToString("n2") + " hPa");
+            Thread.Sleep(2 * 1000);
+
+            UpdateDisplay("MQ-3 air sensor", gasSenseVoltage.ToString("n2") + "%");
+            Thread.Sleep(2 * 1000);
+
+            UpdateDisplay("lightness", lightSensorPercentage.ToString("n2") + "%");
+            Thread.Sleep(2 * 1000);
+
+        }
+
+        private void UpdateDisplay(string line1, string line2 = "")
+        {
+            char_Display.Clear();
+            char_Display.SetCursor(0, 0);
+            char_Display.PrintString(Chop(line1));
+            char_Display.SetCursor(1, 0);
+            char_Display.PrintString(Chop(line2));
+        }
+
+        private string Chop(string text, int max = 16)
+        {
+            if (text.Length > max)
+            {
+                text = text.Substring(0, max);
+            }
+            return text;
         }
     }
 }
